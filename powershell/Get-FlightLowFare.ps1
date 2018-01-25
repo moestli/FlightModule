69fbcdcb5dc1d4cb9c2d9f64e9b42bd8c0ccacab
Magnus Østli <magnus.oestli@gmail.com>
2018-01-25 21:15:29 +0100

    Removed redundant code and fixed Output

--- powershell/Get-FlightLowFare.ps1
+++ powershell/Get-FlightLowFare.ps1
@@ -3,49 +3,35 @@ Function Get-FlightLowFare {
     Param (
         [Parameter(Mandatory)][string]$Origin,
         [Parameter(Mandatory)][string]$Destination,
-        
+        [Parameter(Mandatory=$false)][string]$ReturnCurrency = 'NOK',
+
         [Parameter(Mandatory,HelpMessage="Input must be in format yyyy-MM-dd")]
         [ValidateScript({[datetime]::ParseExact($_,'yyyy-MM-dd',$null)})]
-        [string]$DepartureDate,
-        
-        [Parameter(Mandatory=$false)][string]$ReturnCurrency = 'NOK'
+        [string]$DepartureDate        
     )
     Begin {
         [float]$CurrencyConversion = Get-CurrencyExchangeRate -Currency USD -ReturnRate $ReturnCurrency
-        $Body = @{'apikey'=$AmadeusKey;'origin'=$Origin;'destination'=$Destination;'departure_date'=$DepartureDate}  
+        $Body = @{'apikey'=$AmadeusKey;'origin'=$Origin;'destination'=$Destination;'departure_date'=$DepartureDate}
     }
     Process {
         $Result = Invoke-RestMethod -Method Get -Uri $AmadeusLowFareURL -Body $Body | Select-Object -ExpandProperty results
-        $Output = $Result | ForEach-Object {
+        $Result | ForEach-Object {
             $FlightFare  = $_.fare
             $Itineraries = $_.itineraries.outbound.flights
             foreach ($Itinerary in $Itineraries) {
-                $FlightPrice  = [float]$FlightFare.total_price * $CurrencyConversion
-                $FlightPrice  = [math]::Round($FlightPrice)
-                $FlightNumber = $Itinerary.operating_airline + $Itinerary.flight_number
-                $Aircraft     = $Itinerary.aircraft
-                $FlightClass  = $Itinerary.booking_info.travel_class
-                $FlightDate   = Get-Date $Itinerary.departs_at -Format "dd.MM.yyyy"
-                $FlightTime   = Get-Date $Itinerary.departs_at -Format "HH:mm"
-                $ArrivalDate  = Get-Date $Itinerary.arrives_at -Format "dd.MM.yyyy"
-                $ArrivalTime  = Get-Date $Itinerary.arrives_at -Format "HH:mm"
-                $FlightDuration = New-TimeSpan -Start ($Itinerary.departs_at) -End ($Itinerary.arrives_at)
 
                 New-Object -TypeName psobject -Property @{
-                    'price'           = $FlightPrice;
-                    'flightnumber'    = $FlightNumber;
-                    'aircraft'        = $Aircraft;
-                    'class'           = $FlightClass;
-                    'departure_date'  = $FlightDate;
-                    'departure_time'  = $FlightTime
-                    'arrival_date'    = $ArrivalDate;
-                    'arrival_time'    = $ArrivalTime
-                    'flight_duration' = $FlightDuration 
+                    'price'           = [math]::Round([float]$FlightFare.total_price * $CurrencyConversion);
+                    'flightnumber'    = $Itinerary.operating_airline + $Itinerary.flight_number;
+                    'aircraft'        = $Itinerary.aircraft;
+                    'class'           = $Itinerary.booking_info.travel_class;
+                    'departure_date'  = Get-Date $Itinerary.departs_at -Format "dd.MM.yyyy";
+                    'departure_time'  = Get-Date $Itinerary.departs_at -Format "HH:mm";
+                    'arrival_date'    = Get-Date $Itinerary.arrives_at -Format "dd.MM.yyyy";
+                    'arrival_time'    = Get-Date $Itinerary.arrives_at -Format "HH:mm";
+                    'flight_duration' = New-TimeSpan -Start ($Itinerary.departs_at) -End ($Itinerary.arrives_at)
                 }
             }
         }
-    }
-    End {
-        return $Output
     }
 }
